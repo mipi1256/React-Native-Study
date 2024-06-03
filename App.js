01368d35c71392d1c97f854b494f762af37d14c6
mipi1256 <mipi1258@gmail.com>
2024-06-03 17:13:32 +0900

    #5. 삭제 기능 구현, Pressable 피드백 조건부 렌더링

--- App.js
+++ App.js
@@ -20,8 +20,14 @@ export default function App() {
     // 콜백 함수의 매개값은 해당 상태 변수의 최신 값이 전달됨.
     setTodoGoals((currentTodoGoals) => [
       ...currentTodoGoals,
-      { text: enteredGoalText, key: Math.random().toString() },
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
+  };
+
+  const deleteGoalHandler = (id) => {
+    setTodoGoals((currentTodoGoals) => {
+      return currentTodoGoals.filter((goal) => goal.id !== id);
+    });
   };
 
   return (
@@ -38,10 +44,16 @@ export default function App() {
         <FlatList
           data={todoGoals}
           renderItem={(itemData) => {
-            return <GoalItem text={itemData.item.text} />;
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
           }}
           keyExtractor={(item, index) => {
-            return item.key;
+            return item.id;
           }}
         ></FlatList>
       </View>
